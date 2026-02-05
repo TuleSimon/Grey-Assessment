cdc0382b7dc9d7c64cce5dca7574be827609fa3b
Tule Simon <tulesimon98@gmail.com>
2026-02-05 18:07:44 +0100

    initial

---
+++ app/build.gradle.kts
@@ -0,0 +1,91 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.kotlin.ksp)
+    alias(libs.plugins.google.dagger.hilt.android)
+}
+
+android {
+    namespace = "com.simon.greyassesment"
+
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId =  "com.simon.greyassesment"
+        minSdk = 24
+        targetSdk = 36
+
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.15"
+    }
+
+
+}
+
+dependencies {
+
+    /* -------------------- Core -------------------- */
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.core.splashscreen)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    /* -------------------- Compose -------------------- */
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.bundles.compose)
+    implementation(libs.androidx.compose.runtime)
+    debugImplementation(libs.bundles.compose.debug)
+
+    /* -------------------- Navigation -------------------- */
+    implementation(libs.androidx.navigation.compose)
+
+
+    /* -------------------- Image Loading -------------------- */
+    implementation(libs.bundles.coil)
+
+
+    /* -------------------- Dependency Injection -------------------- */
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
+    ksp(libs.androidx.hilt.compiler)
+
+    /* -------------------- Logging -------------------- */
+    implementation(libs.timber)
+
+    /* -------------------- Testing -------------------- */
+    testImplementation(libs.junit)
+    implementation(libs.kotlinx.collections.immutable)
+
+
+
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+}
---
+++ app/src/main/java/com/simon/greyassesment/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.simon.greyassesment
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.remember
+import com.simon.greyassesment.features.learnpath.screens.BadgeState
+import com.simon.greyassesment.features.learnpath.screens.LearningPathScreen
+import com.simon.greyassesment.features.learnpath.screens.LevelNode
+import com.simon.greyassesment.ui.theme.GreyAssesmentTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GreyAssesmentTheme {
+                val levels = remember {
+                    listOf(
+                        LevelNode(1, "Programming Basics", BadgeState.COMPLETED, R.drawable.badge_completed),
+                        LevelNode(2, "Git & Version Control", BadgeState.COMPLETED,  R.drawable.badge_completed),
+                        LevelNode(3, "Learn React", BadgeState.InProgress(0.5f), R.drawable.badge_in_progress),
+                        LevelNode(4, "Core Mobile UI Build", BadgeState.LOCKED,  R.drawable.badge_not_started),
+                        LevelNode(5, "Access Device Features", BadgeState.LOCKED,  R.drawable.badge_not_started),
+                        LevelNode(6, "Navigations and Forms", BadgeState.LOCKED,  R.drawable.badge_not_started),
+                        LevelNode(7, "State Management", BadgeState.LOCKED,  R.drawable.badge_not_started),
+                    )
+                }
+                LearningPathScreen(levels)
+            }
+        }
+    }
+}
